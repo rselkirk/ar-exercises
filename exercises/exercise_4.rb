93aa02fc4bda32291d077affb57754c116169b87
Rachel Selkirk <rachel.selkirk@gmail.com>
2017-05-09 23:18:28 +0000

    exercise 6 complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -11,22 +11,22 @@ puts "----------"
 st1 = Store.create(
   name: "Surrey",
   annual_revenue: 224000,
-  mens_apparel: true,
+  mens_apparel: false,
   womens_apparel: true,
   )
 
 st2 = Store.create(
   name: "Whistler",
   annual_revenue: 1900000,
-  mens_apparel: false,
-  womens_apparel: true,
+  mens_apparel: true,
+  womens_apparel: false,
   )
 
 st3 = Store.create(
   name: "Yaletown",
   annual_revenue: 430000,
   mens_apparel: true,
-  womens_apparel: false,
+  womens_apparel: true,
   )
 
 @mens_stores = Store.where(mens_apparel: true)
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,24 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store
+  has_many :employees
+end
+
+class Employee 
+  belongs_to :store 
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Captain", last_name: "Cook", hourly_rate: 40)
+
+@store1.employees.create(first_name: "Peter", last_name: "Rabbit", hourly_rate: 50)
+
+@store2.employees.create(first_name: "Roger", last_name: "Rabbit", hourly_rate: 15)
+
+@store2.employees.create(first_name: "Jessica", last_name: "Rabbit", hourly_rate: 30)
+
+@store2.employees.create(first_name: "Anne", last_name: "Shirley", hourly_rate: 45)
+
