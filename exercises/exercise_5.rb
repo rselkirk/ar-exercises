8c5d3b40f1578077fdd4d0e1f2091e65521c62b0
Rachel Selkirk <rachel.selkirk@gmail.com>
2017-05-09 21:51:25 +0000

    exercise 5 complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,21 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_sum = Store.all
+
+count = Store.count
+
+total = 0
+
+@total_sum.each do |store|
+  total += store.annual_revenue
+end
+
+average = total/count
+
+@one_million_revenue = Store.where("annual_revenue > ?", 1000000).count
+
+puts "Total Revenue: #{total}"
+puts "Average: #{average}"
+puts "Stores with 1 million revenue: #{@one_million_revenue}"
