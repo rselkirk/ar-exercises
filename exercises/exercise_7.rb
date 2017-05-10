4fb2794d6ef2312c37d76fe1d050f12e654b1e52
Rachel Selkirk <rachel.selkirk@gmail.com>
2017-05-10 01:09:23 +0000

    exercise 7 complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,23 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  validates_presence_of :first_name, :last_name, :hourly_rate, :store_id
+  validates :hourly_rate, :numericality => {integer: true, greater_than_or_equal_to: 40, less_than: 200}
+end
+
+class Store < ActiveRecord::Base
+  validates_presence_of :name, :annual_revenue
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {integer: true, greater_than: 0}
+end
+
+puts "What store?"
+store_request = gets.chomp
+
+@st = Store.create(
+  name: store_request
+)
+
+puts @st.errors.full_messages if @st.errors.any?
